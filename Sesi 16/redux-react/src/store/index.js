a725ee0ecd39bc68ca153193a5f091d6485991b2
farrelkumara <67863604+farrelkumara@users.noreply.github.com>
2021-10-05 17:17:27 +0700

    Sesi 16 - Latihan Redux cli, web, react
    Johanes Elian Farrel Kumara
    FERD-KI-003

---
+++ Sesi 16/redux-react/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+const initialState = {
+  counter: 0,
+  users: [],
+};
+
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      let substractor = action.payload ? action.payload : 1;
+      return { counter: state.counter + substractor };
+    case "DECREMENT":
+      return { counter: state.counter - 1 };
+    case "SET_USERS":
+      return { users: action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counter, applyMiddleware(thunk, logger));
+
+export default store;
